ee2b7ac1c13e942633ad700d6551ab1185ff8811
sharon.holliday <sharon.holliday@rea-group.com>
2016-09-07 16:54:04 +1000

    Argonaut optional fields examples

--- src/main/scala/com/rea/json/ArgonautExercises.scala
+++ src/main/scala/com/rea/json/ArgonautExercises.scala
@@ -34,7 +34,7 @@ object ArgonautExercises {
     * We can use the @argonaut.Json.apply(fields: (JsonField, Json)*) method, which takes a
     * vararg of tuples (JsonField, Json).
     * JsonField is just a type alias for String.
-    * We are after: '{"surname":"Jones","firstNames":["james","henry"],"principle":true}' NB: ignore agentId for now.
+    * We are after: '{"surname":"Jones","firstNames":["james","henry"],"principal":true}' NB: ignore agentId for now.
     */
   case class Agent(surname: String, firstNames: List[String], principal: Boolean, agentId: Option[String] = None)
 
@@ -42,7 +42,7 @@ object ArgonautExercises {
     Json(
       "surname" -> jString(agent.surname),
       "firstNames" -> jArray(agent.firstNames.map(jString)),
-      "principle" -> jBool(agent.principal)
+      "principal" -> jBool(agent.principal)
     ).nospaces
 
   /** Introducing Codecs
@@ -51,9 +51,9 @@ object ArgonautExercises {
     * Codecs to the rescue!
     * We can construct a tuple of (JsonField, Json) by using the := method.
     * This is a method added on String by aronaut, that uses an implicit encoder on the passed object, to give you back the tuple.
-    *  e.g.
+    * e.g.
     */
-  val myField = "myField" := "some string"  // use your IDE to explore implicit parameters and the := method in this variable definition.
+  val myField = "myField" := "some string" // use your IDE to explore implicit parameters and the := method in this variable definition.
 
   /** Exercise 5
     * Rewrite agent encoding using the codecs style
@@ -62,42 +62,178 @@ object ArgonautExercises {
     Json(
       "surname" := agent.surname,
       "firstNames" := agent.firstNames,
-      "principle" := agent.principal
+      "principal" := agent.principal
     ).nospaces
 
   /**
     * But I have a field that is an Option ... I only want to write it if it exists!
-    * Exercise 6
+    *
     * Option 1: we could use normal scala to create our list of fields, only adding it if it exists.
+    *
+    *
+    * Exercise 6
+    *
+    *
+    * Rewrite agent encoding to also encode the agentid if it exists.
+    * For now, create a List of tuples, only adding the agentid tuple to the list if it is required.
     *
     */
 
+  def writeAgent3(agent: Agent): String = {
+
+    val mandatoryFields: List[(String, Json)] = List(
+      "surname" := agent.surname,
+      "firstNames" := agent.firstNames,
+      "principal" := agent.principal
+    )
+
+    val optionalFields: List[(String, Json)] = agent.agentId.toList.map("agentid" := _)
+
+    Json(
+      (mandatoryFields ++ optionalFields): _*
+    ).nospaces
+
+  }
 
   /**
-    * Exercise 7
-    * Option 2: we could use Argonauts "cons" format to conditionally add fields.
-    * The ->: method on Json is a right associative method that takes a tuple (JsonField, Json)
+    *
+    * Option 2: There are a few new concepts that we can introduce to conditionally add fields in a slightly nicer DSL.
+    * We will need a few new argonaut concepts to do this.
+    *
+    * 1. Cons method of creating objects
+    * ==================================
+    * The ->: method on Json is a right associative method that takes a tuple (JsonField, Json) and adds it to
+    * the jObject, much like in list a :: myList adds an item to a list.
     * We start with a jEmptyObject and add fields to it.
-    */
-
-  val myObject : Json = ("key2" := "hello") ->: jEmptyObject
-
-
-  /** We need a couple of other argonaut methods in our tool kit to add optional fields
-    * First the :=? field constructor:
-    */
-    val myMaybeObject: Option[String] = None
-  val myMaybeField: Option[(String, Json)] = "option" :=? None[String]
-
-  /** And now the ->?: field appender on @argonaut.Json
+    *
+    * e.g. val myObject : Json = ("key1" := "hello") ->: ("key2" := "world") ->: jEmptyObject  // {"key2":"world","key1":"hello"}
+    *
+    * NOTE: it appends rather than prepends the key!
+    *
+    *
+    * 2. Creating optional tuples
+    * ============================
+    * The :=? method is implicitly added to String by Argonaut, it gives us an optional tuple.  (I see it can also be written: :?=  )
+    * def :=?[A: EncodeJson](a: Option[A]):Option[(JsonField, Json)]
+    * val notAnOption: Option[String] = None
+    * val myMaybeField = "option" :=? notAnOption // None
+    * val myMaybeField2 = "option" :=? Some("value") //
+    *
+    *
+    * 3. Optionally adding a tuple to an object
+    * =========================================
+    * And now the ->?: field appender on @argonaut.Json
     * It will only append a option of tuple if the tuple exists.
-    */
-  val myOtherObject: Json = myMaybeField ->?: jEmptyObject
+    * def ->?:(o: Option[(JsonField, Json)]): Json
+    *
+    * (("field1" :=? notAnOption) ->?: jEmptyObject) // {}
+    *
+    * (("field1" :=? Some("my value")) ->?: jEmptyObject).nospaces //{"field1":"my value"}
+    *
+    *
+    *
+    * Exercise 7
+    *
+    *
+    * Rewrite agent encoding using the :=? and ->?: methods
+    *
+    **/
+
+  def writeAgent4(agent: Agent): String = {
+    ("agentid" :=? agent.agentId) ->?:
+    ("principal" := agent.principal) ->:
+    ("firstNames" := agent.firstNames) ->:
+    ("surname" := agent.surname) ->:
+    jEmptyObject
+  }.nospaces
 
 
   /**
-    * Write our own encoders....
+    * Option 3
+    * Use a custom formater, in place of no spaces to strip out optional values when you are converting from Json to a string
+    *  val formatter = PrettyParams.nospace.copy(dropNullKeys = true)
+    *
+    *
+    * Exercise 8
+    *
+    *
+    * Rewrite agent encoding the above pretty formatter (Use the
+    *
+    **/
+
+  val formatter = PrettyParams.nospace.copy(dropNullKeys = true)
+
+  def writeAgent5(agent: Agent): String =
+    Json(
+      "surname" := agent.surname,
+      "firstNames" := agent.firstNames,
+      "principal" := agent.principal,
+      "agentid" := agent.agentId
+    ).pretty(formatter)
+
+
+  /**
+    * Exercise 9 - Writing custom encoders
+    *
+    * Using the := method to find the build in encoders automatically tidied
+    * up our code nicely.   But what if there isn't a build in encoder for our type?
+    *
+    * Consider a property that contains an agent.
+    * We want to write
+    * def writeProperty(property:
+    *
     */
+
+  case class Property(description: String, agent: Agent)
+
+  /**
+    * Write out an encoder that just uses the syntax from Exercise 4,
+    * and a simple encode Agent method.
+    */
+
+
+
+  def writeProperty(property: Property): String = {
+    def encodeAgent(agent: Agent): Json = Json(
+      "surname" -> jString(agent.surname),
+      "firstNames" -> jArray(agent.firstNames.map(jString)),
+      "principal" -> jBool(agent.principal)
+    )
+    Json(
+      "description" := property.description,
+      "agent" -> encodeAgent(property.agent)
+    ).nospaces
+  }
+
+  /**
+    * Exercise 10
+    *
+    * But how do we replace that "-> encodeAgent(...)" so that it implicitly finds it.
+    * Look again at the := method definition
+    * def :=[A: EncodeJson](a: A)
+    *
+    * so we need an EncodeJson[Agent]
+    * EncodeJson is simply a trait, that has a single abstract method:
+    * def encode(a: A): Json
+    *
+    * Argonaut provides a convenience constructor in the EncodeJson object:
+    * def apply[A](f: A => Json): EncodeJson[A]
+    * So we can create and instance by calling EncodeJson[Agent](f: Agent => Json)
+    *
+    * Try again, creating an EncodeJson instance for Agent instread
+    */
+
+  def writePropertyWithEncoder(property: Property): String = {
+    implicit def AgentEncoder = EncodeJson[Agent](agent => Json(
+      "surname" -> jString(agent.surname),
+      "firstNames" -> jArray(agent.firstNames.map(jString)),
+      "principal" -> jBool(agent.principal)
+    ))
+    Json(
+      "description" := property.description,
+      "agent" := property.agent
+    ).nospaces
+  }
 
 
 }
