4617e0927489170edecbc6d62aa3d8e461b84408
sharon.holliday <sharon.holliday@rea-group.com>
2017-08-27 20:43:31 +1000

    Decode Exercises with tests and unimplemented solutions

--- src/test/scala/com/rea/json/CirceDecodeExercisesSpec.scala
+++ src/test/scala/com/rea/json/CirceDecodeExercisesSpec.scala
@@ -2,7 +2,9 @@ package com.rea.json
 
 import com.rea.json.CirceDecodeExercises._
 import io.circe.Json
+import org.specs2.matcher.MatchResult
 import org.specs2.mutable.Specification
+
 
 class CirceDecodeExercisesSpec extends Specification {
 
@@ -13,12 +15,12 @@ class CirceDecodeExercisesSpec extends Specification {
           case Left(error) => ko(s"Error parsing json: $error")
           case Right(json) => json must beEqualTo(Json.fromString("yup, json string"))
         }
+      }
 
-        "report a parsing error" in {
-          parseToJson("""im not json""") match {
-            case Left(error) => ok
-            case Right(json) => ko("should have failed parsing invalid json")
-          }
+      "report a parsing error" in {
+        parseToJson("""im not json""") match {
+          case Left(error) => ok
+          case Right(json) => ko("should have failed parsing invalid json")
         }
       }
     }
@@ -83,19 +85,24 @@ class CirceDecodeExercisesSpec extends Specification {
         val json =
           """
             |{"description" : "a great house",
-            | "agent" : {
-            |   "agentName" : "Wonder Agent",
-            |   "agentId" : "WA1XXX"
-            | }
+            | "agent" : {"surname":"Jones","firstNames":["james","henry"],"principal":true}
             |}
           """.stripMargin
+
         parseDescription(json) match {
           case None => ko("No, it didn't work")
           case Some(description) => description must beEqualTo("a great house")
         }
       }
-    }
 
+      "return none when the json doesn't contain a description field" in {
+        val json = """ {"other_field": 234} """
+        parseDescription(json) match {
+          case None => ok
+          case Some(description) => ko("No, it didn't work")
+        }
+      }
+    }
 
 
     "Exercise 2.1" should {
@@ -103,10 +110,7 @@ class CirceDecodeExercisesSpec extends Specification {
         val json =
           """
             |{"description" : "a great house",
-            | "agent" : {
-            |   "agentName" : "Wonder Agent",
-            |   "agentId" : "WA1XXX"
-            | }
+            | "agent" : {"surname":"Jones","firstNames":["james","henry"],"principal":true}
             |}
           """.stripMargin
         parseToJson(json).map(fetchDescription) match {
@@ -118,23 +122,128 @@ class CirceDecodeExercisesSpec extends Specification {
     }
 
     "Exercise 2.2" should {
-      "extract the agent name" in {
+      "extract the agent surname" in {
         val json =
           """
             |{"description" : "a great house",
-            | "agent" : {
-            |   "agentName" : "Wonder Agent",
-            |   "agentId" : "WA1XXX"
-            | }
+            | "agent" : {"surname":"Jones","firstNames":["james","henry"],"principal":true}
             |}
           """.stripMargin
-        parseToJson(json).flatMap(fetchAgentName) match {
+        parseToJson(json).flatMap(fetchAgentSurname) match {
           case Left(error) => ko(s"failed with: $error")
-          case Right(agentName) => agentName must beEqualTo(Json.fromString("Wonder Agent"))
+          case Right(agentName) => agentName must beEqualTo(Json.fromString("Jones"))
         }
+      }
+    }
+
+    def withAgentJson[A]: (Json => MatchResult[A]) => MatchResult[Any] = {
+      withJson[A](
+        """
+          | {"surname":"Jones","firstNames":["james","henry"],"principal":true}
+        """.stripMargin
+      )
+    }
+
+    "Exercise 3.1" should {
+
+      "return a DecoderFalure when passed a failed cursor" in {
+        withPropertyJson { json =>
+          val failedCursor = json.hcursor.downField("Not a field")
+          cursorResult(failedCursor).left.map(_.message) must beLeft("oopsie")
+          cursorResult(failedCursor).left.map(_.history) must beLeft(failedCursor.history)
+        }
+      }
+      "return a Right('Woo hoo') when passed a successful cursor" in {
+        withPropertyJson { json =>
+          val successCursor = json.hcursor.downField("description")
+          cursorResult(successCursor) must beRight("Woo hoo")
+        }
+      }
+    }
+
+    "Exercise 4.1" should {
+
+      "extract the agent surname" in {
+        withPropertyJson { json =>
+          fetchAgentSurname2(json) must beRight("Jones")
+        }
+      }
+    }
+
+    "Exercise 4.2" should {
+      "extract the agent" in {
+        withAgentJson { json =>
+          decodeAgent(json.hcursor) must beRight(Agent("Jones", List("james", "henry"), true))
+        }
+      }
+    }
+
+    def withPropertyJson[A]: (Json => MatchResult[A]) => MatchResult[Any] = {
+      withJson[A](
+        """
+          |{"description" : "a great house",
+          | "agent" : {"surname":"Jones","firstNames":["james","henry"],"principal":true}
+          |}
+        """.stripMargin
+      )
+    }
+
+    "Exercise 4.3 decodes property json" in {
+      withPropertyJson { json =>
+        decodeProperty(json.hcursor) must beRight(
+          Property("a great house",
+            Agent("Jones", List("james", "henry"), true)
+          ))
+      }
+    }
+
+    "Exercise 4.4 decodes agent with typed Surname" in {
+      withAgentJson { json =>
+        decodeAgent2(json.hcursor) must beRight(
+          Agent2(Surname("Jones"), List("james", "henry"), true)
+        )
+      }
+    }
+
+    "Exercise 4.5" should {
+      "pass when agent is a principal" in {
+        withAgentJson { json =>
+          decodePrincipalAgent(json) must beRight(
+            PrincipalAgent("Jones")
+          )
+        }
+      }
+
+      "fail when agent is not a principal" in {
+        withJson(
+          """
+            |{"description" : "a great house",
+            | "agent" : {"surname":"Jones","firstNames":["james","henry"],"principal":false}
+            |}
+          """.stripMargin) { json =>
+          decodePrincipalAgent(json) must beLeft
+        }
+      }
+    }
+
+    "Exercise 4.6 decodes property with agent json" in {
+      withPropertyJson { json =>
+        decodePropertyWithAgencyJson(json) must beRight(
+          PropertyWithAgentJson(
+            "a great house",
+            """{"surname":"Jones","firstNames":["james","henry"],"principal":true}"""
+          ))
       }
     }
   }
 
+
+  def withJson[A](jsonString: String)(f: Json => MatchResult[A]): MatchResult[Any] = {
+
+    parseToJson(jsonString) match {
+      case Left(error) => ko(s"failed with: $error")
+      case Right(json) => f(json)
+    }
+  }
 
 }
